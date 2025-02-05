7a2125fbc0e535e45ff4a722283fd9b0b40c19b8
Carlos Ataíde <ataide.carlos@outlook.com>
2025-02-05 12:34:00 +0000

    Error in Delete procedure

--- partition_switch/usp_delete_table_partition.sql
+++ partition_switch/usp_delete_table_partition.sql
@@ -13,7 +13,8 @@ CREATE PROCEDURE dbo.usp_delete_table_partition
 AS
 BEGIN
 
-declare @internal_ids table ( batch_id int, partition_exists bit default 0 )
+declare @internal_ids table ( batch_id int, partition_number int default 0, partition_exists bit default 0, completed bit default 0)
+declare @partition_number int
 
 -- WARNING: This is a proof-of-concept. Do not use this code in any production environment without extensive testing and/or re-writing the code.
 --
@@ -37,10 +38,45 @@ begin
 end
 
 
--- Check if the partition exists
+-- Set the flag for existing partitions (and validate the batch_id)
 update @internal_ids set partition_exists = 1
-where batch_id in (
-    select prv.[value]
+    , partition_number = p.partition_number
+from sys.partitions p
+join sys.indexes i on p.object_id = i.object_id and p.index_id = i.index_id
+join sys.partition_schemes ps on ps.data_space_id = i.data_space_id
+join sys.partition_functions pf on pf.function_id = ps.function_id
+left join sys.partition_range_values prv on prv.function_id = pf.function_id 
+    and prv.boundary_id = p.partition_number - 1
+where i.object_id = OBJECT_ID('user_data')
+    and i.index_id <= 1
+    and batch_id = prv.value
+    --and batch_id <> 0
+
+
+-- Switch the partition and delete it
+while exists (select 1 from @internal_ids where completed = 0 and partition_exists = 1)
+begin
+    select @partition_number = partition_number
+        , @batch_id = batch_id
+    from @internal_ids
+    where completed = 0
+        and partition_exists = 1
+
+    alter table user_data switch partition @partition_number to user_data_cleanup partition @partition_number        
+    select 'Switched partition id ' + convert(varchar(10),@partition_number) + ' | batch_id ' + convert(varchar(10),@batch_id) as 'Result'
+
+    alter partition function pf_batch_id() merge range(@batch_id)
+    select 'Merged range (batch_id) ' + convert(varchar(10),@batch_id) as 'Result'
+
+    -- Truncate the target table AFTER the switch
+    truncate table user_data_cleanup
+
+    --  Mark the partition as completed
+    update @internal_ids set completed = 1 where batch_id = @batch_id and partition_number = @partition_number
+
+
+    -- Partition number is dynamic, so we need to update it after each partition switch
+    update @internal_ids set partition_number = p.partition_number
     from sys.partitions p
     join sys.indexes i on p.object_id = i.object_id and p.index_id = i.index_id
     join sys.partition_schemes ps on ps.data_space_id = i.data_space_id
@@ -49,48 +85,11 @@ where batch_id in (
         and prv.boundary_id = p.partition_number - 1
     where i.object_id = OBJECT_ID('user_data')
         and i.index_id <= 1
-    )
-
-
--- Switch the partition and delete it
-if (select count(partition_exists) from @internal_ids where partition_exists = 1) > 0
-begin
-
-    -- Truncate the target table BEFORE the switch
-    truncate table user_data
-
-
-    declare cursor_batch_id cursor for
-    select batch_id from @internal_ids where partition_exists = 1
-
-    open cursor_batch_id
-    fetch next from cursor_batch_id into @id
-
-    while @@FETCH_STATUS = 0
-    begin
-
-        alter table user_data switch partition @id to user_data_cleanup partition @id        
-        select 'Switched batch_id ' + convert(varchar(10),@id) as 'Result'
-
-        alter partition function pf_batch_id() merge range(@id)
-        select 'Deleted partition batch_id ' + convert(varchar(10),@id) as 'Result'
-
-        fetch next from cursor_batch_id into @id
-    end
-
-    close cursor_batch_id
-    deallocate cursor_batch_id
-
-
-    -- Truncate the target table AFTER the switch
-    truncate table user_data
+        and batch_id = prv.value
+        --and batch_id <> 0
 
 end
 
 
-
 END
 GO
-
--- EXECUTE dbo.usp_delete_table_partition 'user_data', 1
--- GO
